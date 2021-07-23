5884be0373b6e3dfa50bb754c2cae9217b3ed453
codesktop <codesktop@foxmail.com>
2021-07-23 16:21:07 +0800

    init commit

--- melon-daily.js
+++ melon-daily.js
@@ -1,8 +1,8 @@
 const melon = require('obliviate/lib/tasks/melon')
 
 async function melonDaily() {
-  await melon.perform({ id: '32185128', binId: '5ffdc4f368f9f835a3de4718' })
-  await melon.perform({ id: '33382021', binId: '60714a9fceba857326722071' })
+  await melon.perform({ id: '32185128', binId: 'kzws2arEhg' })
+  await melon.perform({ id: '33382021', binId: 'KiHUNpZCbx' })
 }
 
 melonDaily()
